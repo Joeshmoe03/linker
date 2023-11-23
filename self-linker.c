bbd82d04f203f1e4a8705a153b8df676f82a3646
Solano <hsolano@weathertop.cs.middlebury.edu>
2023-11-23 18:20:34 -0500

    progress

--- self-linker.c
+++ self-linker.c
@@ -1,6 +1,31 @@
 /* self-linker.c */
 #include <stdio.h>
+#include <dlfcn.h>
+#include <stdlib.h>
+#include <gnu/lib-names.h>
 
 
-extern void *_GLOBAL_OFFSET_TABLE_;
+int main(int argc, char *argv[]) {
+	extern void *_GLOBAL_OFFSET_TABLE_;
+	int (*putsreturn)(char *);//function prototype for puts
+
+	/* code modifying GOT table for puts, before it's actually called */
+
+	void *handle;
+
+	handle = dlopen(LIBM_SO, RTLD_LAZY);
+
+	// putsreturn = (int (*)(int)) dlsym(handle, "puts");
+	*(void **) (&putsreturn) = dlsym(handle, "puts");
+
+	printf("%p\n",handle);
+	printf("%p\n",_GLOBAL_OFFSET_TABLE_);
+	printf("%d\n", (*putsreturn)("helloworld"));
+
+	if(!handle) {
+		fprintf(stderr, "%s\n", dlerror());
+		exit(EXIT_FAILURE);
+	}
+
+}
 
