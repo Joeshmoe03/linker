bf58e655932b8f8c16c7aa62f3979b4be03a8fe3
Solano <hsolano@weathertop.cs.middlebury.edu>
2023-11-23 22:49:33 -0500

    maybe?

--- self-linker.c
+++ self-linker.c
@@ -4,28 +4,20 @@
 #include <stdlib.h>
 #include <gnu/lib-names.h>
 
-
 int main(int argc, char *argv[]) {
 	extern void *_GLOBAL_OFFSET_TABLE_;
-	int (*putsreturn)(char *);//function prototype for puts
-
-	/* code modifying GOT table for puts, before it's actually called */
-
 	void *handle;
+	void *putslibcaddr;
+	void *putsgotplt;
 
-	handle = dlopen(LIBM_SO, RTLD_LAZY);
+	/* Code modifying GOT table for puts, before it's actually caled*/
+	handle = dlopen(LIBC_SO, RTLD_LAZY);
+	putslibcaddr = dlsym(handle, "puts");
+	putsgotplt = (void *)0x404000; /* # 0x404000 <puts@got.plt> from gdb */
+	//void *toChange = (void*)((__intptr_t)(_GLOBAL_OFFSET_TABLE_) + 0x1f8); //yep got us to the right place, whatever is saved at 
+	*(void **)putsgotplt = putslibcaddr;
 
-	// putsreturn = (int (*)(int)) dlsym(handle, "puts");
-	*(void **) (&putsreturn) = dlsym(handle, "puts");
-
-	printf("%p\n",handle);
-	printf("%p\n",_GLOBAL_OFFSET_TABLE_);
-	printf("%d\n", (*putsreturn)("helloworld"));
-
-	if(!handle) {
-		fprintf(stderr, "%s\n", dlerror());
-		exit(EXIT_FAILURE);
-	}
-
+	/* puts call */
+	puts("helloworld");
 }
 
