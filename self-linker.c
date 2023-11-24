f69e5b61b659e3f3de96dad52e214295dc1d9e4f
Solano <hsolano@weathertop.cs.middlebury.edu>
2023-11-24 02:09:07 -0500

    prettier and more pointy

--- self-linker.c
+++ self-linker.c
@@ -13,7 +13,9 @@ int main(int argc, char *argv[]) {
 	void *handle;
 	char* lib = LIBC_SO;
 	char* sym = "puts";
-	void* GOTentry;
+	void* symaddress;
+	int symgotpltval;
+	void *symgotpltaddr;
 
 	/* If dlopen fails, exit with error, else continue */
 	handle = dlopen(lib, RTLD_LAZY);
@@ -23,13 +25,21 @@ int main(int argc, char *argv[]) {
 	}
 
 	/* We pass our handle to dlsym to resolve symbol of puts as an address */
-	GOTentry = dlsym(handle, sym);
-	if(GOTentry == NULL) {
+	symaddress = dlsym(handle, sym);
+	if(symaddress == NULL) {
 		perror("dlsym");
 		exit(1);
 	}
 
+	/* Hardcode/magic number explanation: We obtained the PLT entry for puts using the following commands: 
+	 # 404000 <puts@GLIBC_2.2.5> running objdump -j .plt -d self-linker, 
+	 same as # 0x404000 <puts@got.plt> from gdb using breakpoints and disassembling 
+	 This is the same result of as doing symgotpltaddr = (void *)0x404000;
+	 */
+
+	symgotpltval = 0x404000;
+	symgotpltaddr = (void*)((__intptr_t)(GOTtable) + (symgotpltval - (__intptr_t)(GOTtable)));
+	*(void **)symgotpltaddr = symaddress;
 	puts("Hello world!");
-	puts("Second message!");
 	return 0;
 }
